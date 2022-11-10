ffd0b06561935b974f9e7d088f38c3f80d7b66ac
Elias Maier <maierelias13@gmai.com>
2022-11-10 11:45:10 +0100

    ignore elm/elm-stuff

---
+++ rust/target/debug/deps/rust-45c960685fa69110.d
@@ -0,0 +1,5 @@
+/home/elias/code/func-checker/rust/target/debug/deps/rust-45c960685fa69110.rmeta: src/main.rs
+
+/home/elias/code/func-checker/rust/target/debug/deps/rust-45c960685fa69110.d: src/main.rs
+
+src/main.rs:
