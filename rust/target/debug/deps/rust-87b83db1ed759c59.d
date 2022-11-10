25dc96c1931e72c842cda2d774bdd3f382fc3912
Elias Maier <maierelias13@gmai.com>
2022-11-10 11:46:51 +0100

    ignore rust/target

--- rust/target/debug/deps/rust-87b83db1ed759c59.d
+++
@@ -1,5 +0,0 @@
-/home/elias/code/func-checker/rust/target/debug/deps/rust-87b83db1ed759c59.rmeta: src/main.rs
-
-/home/elias/code/func-checker/rust/target/debug/deps/rust-87b83db1ed759c59.d: src/main.rs
-
-src/main.rs:
